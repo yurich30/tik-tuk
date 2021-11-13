1a2e050084d5a45e8ef4060dc7e90f63683f84ef
Yurii <yurich3098@gmail.com>
2021-11-13 21:47:18 +0200

    setup eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,27 @@
-import React, {useEffect} from 'react';
-import axios from "axios";
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const App: React.FC = () => {
-
   useEffect(() => {
     const options = {
       method: 'GET',
       url: 'https://tiktok33.p.rapidapi.com/user/feed/dave.xp',
       headers: {
         'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
-        'x-rapidapi-key': '917d761687msha6ac156d38ce065p1a07fcjsnf7fe4f79a896'
-      }
+        'x-rapidapi-key': '294f036434msh90cf68bd9dd8898p152614jsn805ac4221ef0',
+      },
     };
 
-    (async () => {
+    const getData = async () => {
       // @ts-ignore
-      const res = await axios.request(options)
-      console.log(res)
-    })()
-    // @ts-ignore
-    // axios.request(options).then(function (response) {
-    //   console.log(response.data);
-    // }).catch(function (error) {
-    //   console.error(error);
-    // });
+      const res = await axios.request(options);
+      console.log(res.data);
+    };
 
-    console.log('useEffect')
-  })
+    getData();
+  });
 
-  return (
-    <div>
-      TikTuk
-    </div>
-  )
-}
+  return <div>TikTuk</div>;
+};
 
 export default App;
